54cdda78aee8eb2543892a5e2e0e17e46ecac29f
halexand <harriet.xander@gmail.com>
2019-02-14 19:02:54 -0500

    added submit_snakemake

--- submit_script/submit_snakemake.sh
+++ submit_script/submit_snakemake.sh
@@ -1,4 +1,4 @@
 snakemake   \
         --jobs 40 --use-conda --profile tara-metag  \
-        --cluster-config cluster.yaml --cluster " echo sbatch --parsable  --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem} --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
+        --cluster-config cluster.yaml --cluster " echo sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem} --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
 
